f42ea4a42354ede5c602331ca700ac3dcd2a42d2
Abolfazl Asadi <abolfazlasadiev@users.noreply.github.com>
2026-06-02 13:19:42 +0330

    new

---
+++ category-01-basics-console/HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+    // Attribute
+    private String message;
+
+    // Constructor
+    public HelloWorld() {
+        message = "Hello, World!";
+    }
+
+    // Method
+    public void printMessage() {
+        System.out.println(message);
+    }
+}
