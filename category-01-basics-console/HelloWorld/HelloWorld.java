a6415e547c986663733b93d19a4f8616ce0f9f73
Abolfazl Asadi <abolfazlasadiev@users.noreply.github.com>
2026-07-01 07:50:17 +0330

    Add new change

--- category-01-basics-console/HelloWorld/HelloWorld.java
+++ category-01-basics-console/HelloWorld/HelloWorld.java
@@ -1,15 +1,12 @@
 
 public class HelloWorld {
 
-    // Attribute
     private String message;
 
-    // Constructor
     public HelloWorld() {
         message = "Hello, World!";
     }
 
-    // Method
     public void printMessage() {
         System.out.println(message);
     }
--- category-01-basics-console/HelloWorld/Main.java
+++ category-01-basics-console/HelloWorld/Main.java
@@ -3,10 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Create object
         HelloWorld hello = new HelloWorld();
 
-        // Call method
         hello.printMessage();
     }
 }
